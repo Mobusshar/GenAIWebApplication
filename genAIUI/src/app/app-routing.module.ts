46317e15301ac785c95bbfab5f9074d15e6d4d65
MOBUSSHAR ISLAM <mi.tonmoy4@gmail.com>
2025-02-26 22:07:40 +0200

    Update

--- genAIUI/src/app/app-routing.module.ts
+++ genAIUI/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ImageGeneratorComponent } from './image-generator/image-generator.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 const routes: Routes = [
-  { path: 'image-generator', component: ImageGeneratorComponent },
-  { path: '', redirectTo: '/image-generator', pathMatch: 'full' }
+  { path: '', component: LandingPageComponent },
+  { path: 'image-generator', component: ImageGeneratorComponent }
+  //{ path: '', redirectTo: '/image-generator', pathMatch: 'full' }
 ];
 
 @NgModule({
--- genAIUI/src/app/landing-page/landing-page.component.ts
+++ genAIUI/src/app/landing-page/landing-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-landing-page',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./landing-page.component.css']
 })
 export class LandingPageComponent {
+  constructor(private router: Router) {}
 
+  navigateToExercise(exercise: number) {
+    if (exercise === 2) {
+      this.router.navigate(['/image-generator']);
+    } else {
+      alert('Exercise 1 is not implemented yet.');
+    }
+  }
 }
